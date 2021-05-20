f9c69af7927fd52adebccecd5c5759172af4c6ee
Artem <logik562@ya.ru>
2021-05-20 23:53:33 +0700

    1

---
+++ script.js
@@ -0,0 +1,14 @@
+var catalog = [XL1200C = {nam: 'XL1200C', price: 5500}, seat = {nam: 'seat', price: 500}, exhaust = {nam: 'exhaust', price: 300}];
+var korzina = [];
+
+
+function countBasketPrice() {
+    var summa = 0;
+    for (var i=0; i<korzina.length; i++) {
+        summa += catalog[korzina[i]].price;
+    }
+    return summa;
+}
+
+
+alert('К оплате: ' + countBasketPrice() + '$');
